ab672e11f4fe481a324b81b51f1bb4c933ac80f5
kwchi <144099685+kwchi@users.noreply.github.com>
2024-11-29 00:21:52 +0200

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "lab-my-tf-state"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "lab-my-tf-lockid"
+  }
+}
+
+# Configure the AWS provider
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+variable "REPOSITORY_URI" {
+  type = string
+}
+
+resource "aws_lightsail_container_service" "flask_application" {
+  name = "flask-app"
+  power = "nano"
+  scale = 1
+
+  private_registry_access {
+    ecr_image_puller_role {
+      is_active = true
+    }
+  }
+
+
+  tags = {
+    version = "1.0.0"
+  }
+}
+
+resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
+  container {
+    container_name = "flask-application"
+
+    image = "${var.REPOSITORY_URI}:latest"
+    
+    ports = {
+      # Consistent with the port exposed by the Dockerfile and app.py
+      8080 = "HTTP"
+    }
+  }
+
+  public_endpoint {
+    container_name = "flask-application"
+    # Consistent with the port exposed by the Dockerfile and app.py
+    container_port = 8080
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 5
+      path                = "/"
+      success_codes       = "200-499"
+    }
+  }
+
+  service_name = aws_lightsail_container_service.flask_application.name
+}
